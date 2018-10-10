9b2fff44c265e97b5816782a2adbce829304c28e
Nicola La Rocca <nicola@kalsyfer.com>
2018-10-10 18:54:59 +0200

    Modifiche Varie Cache

--- src/main/java/com/xantrix/webapp/service/ArticoliServiceImpl.java
+++ src/main/java/com/xantrix/webapp/service/ArticoliServiceImpl.java
@@ -3,7 +3,10 @@ package com.xantrix.webapp.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,20 +29,21 @@ public class ArticoliServiceImpl implements ArticoliService
 	}
 
 	@Override
-	@Cacheable("SelByDescrizione")
+	//@Cacheable("SelByDescrizione")
 	public List<Articoli> SelByDescrizione(String descrizione, Pageable pageable)
 	{
 		return articoliRepository.findByDescrizioneLike(descrizione, pageable);
 	}
 
 	@Override
-	@Cacheable("SelByDescrizione")
+	@Cacheable(value = "articolicache", sync = true)
 	public List<Articoli> SelByDescrizione(String descrizione)
 	{
 		return articoliRepository.findByDescrizioneLike(descrizione);
 	}
 	
 	@Override
+	@Cacheable(value = "articolo",key = "#codArt",sync = true)
 	public Articoli SelByCodArt(String codArt)
 	{
 		return articoliRepository.findByCodArt(codArt);
@@ -47,6 +51,9 @@ public class ArticoliServiceImpl implements ArticoliService
 
 	@Override
 	@Transactional
+	@Caching(evict = { 
+	@CacheEvict(cacheNames="articolicache", allEntries = true),
+	@CacheEvict(cacheNames="articolo",key = "#articolo.codArt")})
 	public void DelArticolo(Articoli articolo)
 	{
 		articoliRepository.delete(articolo);
@@ -54,6 +61,9 @@ public class ArticoliServiceImpl implements ArticoliService
 
 	@Override
 	@Transactional
+	@Caching(evict = { 
+		@CacheEvict(cacheNames="articolicache", allEntries = true),
+		@CacheEvict(cacheNames="articolo",key = "#articolo.codArt")})
 	public void InsArticolo(Articoli articolo)
 	{
 		articoliRepository.save(articolo);
